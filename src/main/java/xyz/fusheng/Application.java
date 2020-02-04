c5fe53a984fa2b6de74e93bed9d4e7844dbfceb4
zhfushengx2048 <53218036+zhfushengx2048@users.noreply.github.com>
2020-02-04 13:06:41 +0800

    1

--- src/main/java/xyz/fusheng/Application.java
+++ src/main/java/xyz/fusheng/Application.java
@@ -15,6 +15,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+// 通过扫包方式，将 MyBatis 相关对象交给 Spring 容器来管理。
 @MapperScan("xyz.fusheng.repository")
 public class  Application {
     public static void main(String[] args) {
